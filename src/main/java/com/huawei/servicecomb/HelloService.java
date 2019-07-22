39d451a45824632a987cd82e528f0e535bdd78e5
dean.liu <ucan1984@163.com>
2019-07-22 21:19:38 +0800

    Add files via upload

---
+++ src/main/java/com/huawei/servicecomb/HelloService.java
@@ -0,0 +1,17 @@
+package com.huawei.servicecomb;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.ws.rs.core.MediaType;
+
+@RestSchema(schemaId = "hello")
+@RequestMapping(path = "/hello", produces = MediaType.APPLICATION_JSON)
+public class HelloService {
+	@RequestMapping(path = "/sayhi", method = RequestMethod.POST)
+	public String sayHi(@RequestParam(name = "name") String name) {
+		return "Hello " + name;
+	}
+}
